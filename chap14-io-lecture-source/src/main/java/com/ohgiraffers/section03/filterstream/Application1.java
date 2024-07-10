e0f2a955147ff7db83eacde9cef2f5f83edf0c22
byunjunsig <ofice89@gmail.com>
2024-07-10 11:51:36 +0900

    다시한번 희망을 가져봐?

---
+++ chap14-io-lecture-source/src/main/java/com/ohgiraffers/section03/filterstream/Application1.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.section03.filterstream;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        /* 수업목표. BufferedWriter와 BufferedReader에 대해 이해하고 사용할 수 있다. */
+
+        /* 필기. java.io 패키지의 입출력 스트림은 크게 기본 스트림과 필터 스트림으로 분류된다.
+         *  기본 스트림은 외부 데이터에 직접 연결되는 스트림이고,
+         *  필터 스트림은 외부 데이터에 직접 연결하지 않고 기본 스트림에 추가로 사용할 수 있는 스트림이다.
+         *  주로 성능을 향상시키는 목적으로 사용되며 생성자를 보면 구분이 가능하다.
+         *  생성자쪽에 매개변수로 다른 스트림을 이용하는 클래스는 필터 스트림이라고 볼수 있다.
+         * */
+
+        /* 설명. 버퍼(buffer)를 이용해서 성능을 향상시키는 보조스트림
+         *  BufferedWriter / BufferedReader
+         * */
+    }
+
+}
